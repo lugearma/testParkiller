75e2b6f7afa43914913ef8b41d07c6f1aa072b96
Luis Arias <lugearma@gmail.com>
2016-08-05 13:25:44 -0500

    Initial Commit

---
+++ testParkiller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testParkiller
+//
+//  Created by Luis Arias on 05/08/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
