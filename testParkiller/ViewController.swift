a3dfb4e526d081c36795e63c0ce5561cbbfda55b
Luis Arias <lugearma@gmail.com>
2016-08-05 23:32:00 -0500

    show user location in the map

--- testParkiller/ViewController.swift
+++ testParkiller/ViewController.swift
@@ -12,28 +12,38 @@ import GoogleMaps
 class ViewController: UIViewController {
     
     var locationManager : CLLocationManager?
+    var camera: GMSCameraPosition?
+    let zoom: Float = 15.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        locationManager = CLLocationManager()
-        locationManager?.delegate = self
-        locationManager?.requestAlwaysAuthorization()
+//        locationManager = CLLocationManager()
+//        locationManager?.delegate = self
+//        locationManager?.requestAlwaysAuthorization()
     }
     
     override func loadView() {
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.9, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: camera)
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        let userLat = self.locationManager?.location?.coordinate.latitude
+        let userLon = self.locationManager?.location?.coordinate.longitude
+        
+        self.camera = GMSCameraPosition.cameraWithLatitude(userLat!, longitude: userLon!, zoom: self.zoom)
+        let mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: self.camera!)
         
         mapView.myLocationEnabled = true
         self.view = mapView
         
-        createMarker(mapView)
+        //createMarker(mapView, latitude: userLat!, longitude: userLon!)
     }
     
-    func createMarker(map: GMSMapView) {
+    func createMarker(map: GMSMapView, latitude lat: CLLocationDegrees, longitude lon: CLLocationDegrees) {
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: lon)
         marker.title = "Sidney"
         marker.snippet = "Australia"
         marker.map = map
@@ -44,9 +54,15 @@ extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == .AuthorizedWhenInUse {
-            print("latitude: \(manager.location?.coordinate.latitude)")
-            print("logintude: \(manager.location?.coordinate.longitude)")
+            self.locationManager?.startUpdatingLocation()
         }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lat = locations.first?.coordinate.latitude
+        let lon = locations.first?.coordinate.longitude
+        print("Location: \(lat), \(lon)")
+        self.camera = GMSCameraPosition.cameraWithLatitude(lat!, longitude: lon!, zoom: self.zoom)
     }
 }
 
