8ab24030f78c8a8d30fb72fd00850301710e5867
Luis Arias <lugearma@gmail.com>
2016-08-06 10:49:59 -0500

    Fixed loadView

--- testParkiller/ViewController.swift
+++ testParkiller/ViewController.swift
@@ -16,31 +16,33 @@ class ViewController: UIViewController {
     var mapView: GMSMapView!
     let zoom: Float = 15.0
     
-    override func loadView() {
+    var userLat: CLLocationDegrees?
+    var userLon: CLLocationDegrees?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         self.locationManager = CLLocationManager()
         self.locationManager?.delegate = self
+//        self.locationManager?.requestWhenInUseAuthorization()
         self.locationManager?.requestAlwaysAuthorization()
         
-        if CLLocationManager.locationServicesEnabled() {
-            
-            self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
-        
-            let userLat = self.locationManager?.location?.coordinate.latitude
-            let userLon = self.locationManager?.location?.coordinate.longitude
-            
-            self.camera = GMSCameraPosition.cameraWithLatitude(userLat!, longitude: userLon!, zoom: self.zoom)
-            self.mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: self.camera!)
-            
-            self.mapView.delegate = self
-            self.mapView.myLocationEnabled = true
-            
-            self.view = mapView
+        self.userLat = self.locationManager?.location?.coordinate.latitude
+        self.userLon = self.locationManager?.location?.coordinate.longitude
 
-        } else {
-            print("En problemas :(")
-//            self.showModal()
-        }
+        print("\(userLat), \(userLon)")
+
+    }
+    
+    func createSearchBar() -> UIView{
+        
+        let frame = CGRect(x: 0.0, y: 0.0, width: 320.0, height: 64.0)
+        
+        let searchBar = UISearchBar(frame: frame)
+        searchBar.delegate = self
+        searchBar.placeholder = "Search place"
+//        searchBar.becomeFirstResponder()
+        return searchBar
     }
     
     func showModal() {
@@ -54,31 +56,65 @@ class ViewController: UIViewController {
     func createMarker(map: GMSMapView, latitude lat: CLLocationDegrees, longitude lon: CLLocationDegrees) {
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: lat, longitude: lon)
-//        marker.title = "Sidney"
-//        marker.snippet = "Australia"
         marker.map = map
     }
 }
 
+//MARK: Location Manager Delegate Methods
+
 extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        if status == .AuthorizedWhenInUse {
+        if status == .AuthorizedAlways {
+            
+            self.userLat = self.locationManager?.location?.coordinate.latitude
+            self.userLon = self.locationManager?.location?.coordinate.longitude
+            
+            self.camera = GMSCameraPosition.cameraWithLatitude(userLat!, longitude: userLon!, zoom: self.zoom)
+            
+            self.mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: self.camera!)
+            
+            self.mapView.delegate = self
+            self.mapView.myLocationEnabled = true
+            
+            self.view = mapView
+            
             self.locationManager?.startUpdatingLocation()
         }
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let lat = locations.first?.coordinate.latitude
-        let lon = locations.first?.coordinate.longitude
-        print("Location: \(lat), \(lon)")
-        self.camera = GMSCameraPosition.cameraWithLatitude(lat!, longitude: lon!, zoom: self.zoom)
+        self.userLat = locations.first?.coordinate.latitude
+        self.userLon = locations.first?.coordinate.longitude
+        print("Location: \(self.userLat!), \(self.userLon!)")
+        self.camera = GMSCameraPosition.cameraWithLatitude(self.userLat!, longitude: self.userLon!, zoom: self.zoom)
     }
 }
 
+//MARK: Google Maps Delegate Methods
+
 extension ViewController: GMSMapViewDelegate {
+    
     func mapView(mapView: GMSMapView, didTapAtCoordinate coordinate: CLLocationCoordinate2D) {
         self.createMarker(self.mapView, latitude: coordinate.latitude, longitude: coordinate.longitude)
+        
+        let startPoint = CLLocationCoordinate2D(latitude: self.userLat!, longitude: self.userLon!)
+        
+        let finalPoint = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        
+        let distance = GMSGeometryDistance(startPoint, finalPoint)
+        
+        print("Distance: \(distance) m")
+        
+    }
+}
+
+//MARK: Search Bar Delegate Methods
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        print("editando texto")
+        return true
     }
 }
 
